99069de76a53705cd2107425c7e4c706ca51188d
redarc <loveredarcmm@gmail.com>
2013-10-17 09:53:05 +0800

    <update> can support insert code in cursor position

--- src/jcattestcaseassit/popup/actions/AttachAction.java
+++ src/jcattestcaseassit/popup/actions/AttachAction.java
@@ -18,17 +18,24 @@ import org.eclipse.jdt.core.WorkingCopyOwner;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.ImportDeclaration;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.PackageDeclaration;
 import org.eclipse.jdt.core.dom.QualifiedName;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.texteditor.ITextEditor;
 
 public class AttachAction implements IObjectActionDelegate {
 
@@ -86,15 +93,33 @@ public class AttachAction implements IObjectActionDelegate {
 	    // Create working copy
 		try {
 			
-			/*
+			int offset = 0;
+			IEditorPart editor =  PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+			if (editor instanceof ITextEditor) {
+			  ISelectionProvider selectionProvider = ((ITextEditor)editor).getSelectionProvider();
+			  ISelection selection = selectionProvider.getSelection();
+			  if (selection instanceof ITextSelection) {
+			    ITextSelection textSelection = (ITextSelection)selection;
+			    offset = textSelection.getOffset(); // etc.
+			  }
+			}
+			
 			ICompilationUnit workingCopy = originalUnit.getWorkingCopy(new WorkingCopyOwner(){}, null);
 
 			// Modify buffer and reconcile
 		    IBuffer buffer = ((IOpenable)workingCopy).getBuffer();
 		    
-		    IType curClass = workingCopy.getTypes()[0]; 
+		    String content = buffer.getContents();
+
+		    String headContent = content.substring(0, offset);
+		    String tailContent = content.substring(offset, content.length());
 		    
-		    buffer.append("class X {}");
+		    String newContent = headContent + "public void dosomething(){}" + tailContent;
+		    buffer.setContents(newContent);
+		    //buffer.replace(0, newContent.length(), newContent);
+		    //IType curClass = workingCopy.getTypes()[0]; 
+		    
+		    //buffer.append("class X {}");
 		    
 		    workingCopy.reconcile(ICompilationUnit.NO_AST, false, null, null);
 		    
@@ -103,7 +128,8 @@ public class AttachAction implements IObjectActionDelegate {
 		    
 		    // Destroy working copy
 		    workingCopy.discardWorkingCopy();
-		    */
+			
+			/*
 			AST ast = AST.newAST(AST.JLS4);
 			CompilationUnit unit = ast.newCompilationUnit();
 			PackageDeclaration packageDeclaration = ast.newPackageDeclaration();
@@ -153,6 +179,7 @@ public class AttachAction implements IObjectActionDelegate {
 			methodDeclaration.setBody(block);
 			type.bodyDeclarations().add(methodDeclaration);
 			unit.types().add(type);
+			*/
 		} catch (JavaModelException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
